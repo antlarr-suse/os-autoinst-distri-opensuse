75af81dfb234fe5e69d278696e5b74e29fc9dc2e
openQA web UI <openqa@chenzi-qe1>
2022-04-27 13:13:59 +0200

    Move active python2 module to module scc_deregistration
    The current workaround still has a problem. As long as it calls
    scc_deregistraion, it would install python2 module, for migration target
    veriosn is SLES15SP3 cases. The bsc#189543 happens after migration and
    deregister the OS, we need to add the workaround when needing to deregister
    the OS after migration.

--- tests/console/scc_deregistration.pm
+++ tests/console/scc_deregistration.pm
@@ -14,6 +14,12 @@ sub run {
     return unless (get_var('SCC_REGISTER') || get_var('HDD_SCC_REGISTERED'));
 
     select_console 'root-console';
+    if ((check_var('UPGRADE_TARGET_VERSION', '15-SP3')) && (is_sle('15-SP1+'))) {
+        # Workaround for bsc#1189543, need register python2 before de-register system
+        record_soft_failure 'bsc#1189543 - Stale python2 module blocks de-registration after system migration';
+        add_suseconnect_product('sle-module-python2');
+    }
+
     scc_deregistration;
 }
 
